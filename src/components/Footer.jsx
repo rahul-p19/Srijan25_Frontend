cf86f8a512493b4dcabd31b9bc8f082fae1d30de
rahul-p19 <pandeyrahul.work@gmail.com>
2025-01-27 05:15:09 +0000

    navbar, footer, hero(animations left) added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='grid grid-cols-5 border-t border-t-white w-screen max-w-full'>
+      <div></div>
+      <div className='col-span-3 border-l border-r border-l-white border-r-white flex flex-col p-3'>
+        <div className='flex justify-between p-8'>
+          <div className='flex flex-col gap-y-4'>
+            <h2 className='text-3xl'>Explore</h2>
+            <div className='flex flex-col text-left text-lg underline underline-offset-2'>
+              <a href='/'>Home</a>
+              <a href='/events'>Events</a>
+              <a href='/merchandise'>Merchandise</a>
+              <a href='/ambassador'>Ambassador</a>
+            </div>
+          </div>
+          <div className='flex flex-col gap-y-4'>
+            <h2 className='text-3xl'>Legal</h2>
+            <div className='flex flex-col text-right underline underline-offset-2 text-lg'>
+              <a href='/privacy'>Privacy</a>
+              <a href='/terms'>Terms</a>
+              <a href='/event'>Event</a>
+              <a href='/sponsor'>Sponsor</a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className='text-sm'>&copy; 2025 - F.E.T.S.U. Presents Srijan, Jadavpur University, Kolkata. All rights reserved.</p>
+        </div>
+      </div>
+      <div></div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='h-[90vh] w-screen max-w-full grid grid-cols-5 relative'>
+      <div></div>
+      <div className='col-span-3 grid grid-cols-2 grid-rows-6 border-l border-r border-l-white border-r-white'>
+        <div className='border-r border-b border-white flex items-end row-span-2'>
+          <img src='/inverted-commas.svg' className='p-4 border-white border-t border-r' />
+        </div>
+        <div className='border-white border-b row-span-2'></div>
+        <div className='border-white border-b border-r flex items-end p-6 text-2xl'>
+          <h3>Coding</h3>
+        </div>
+        <div className='border-white border-b flex justify-end items-end p-6 text-2xl'>
+          <h3>F5 Talks</h3>
+        </div>
+        <div className='row-span-3 border-r-white border-r flex p-6'>
+        </div>
+        <div className='row-span-3 flex items-end justify-end'>
+          <a className='p-3 border-white border-t border-l text-2xl z-[101]' href='/register'>Register Now</a>
+        </div>
+      </div>
+      <div></div>
+      <div className='absolute z-[90] bottom-0 left-[5%] h-[30%] bg-background border border-white'>
+        <img src='/fetsu-presents-srijan-glitch.svg' className='h-full' />
+      </div>
+      <div className='absolute p-3 z-[90] bottom-[20%] right-[12%] bg-background border border-white'>
+        <p className='text-3xl'><span className='text-transparent bg-clip-text bg-gradient-to-r from-blue via-red to-red'>20-23</span> MARCH 2025</p>
+      </div>
+      <img src='/mascot.svg' className='absolute z-[100] top-[20%] left-[50%] -translate-x-[50%] mascot-animation' />
+    </div>
+  )
+}
+
+export default Hero
