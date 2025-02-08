8e3cfd93c35553f12609002b6ec804a5b34017e1
Lofty-Brambles <100270817+Lofty-Brambles@users.noreply.github.com>
2025-02-09 01:47:25 +0530

    Feat/dashboard-page (#5)
    * feat: add instructions for BrowserRouter redirects
    
    * Revert "feat: add instructions for BrowserRouter redirects"
    
    This reverts commit 1b7bd4befd31e82dbce478a05763d8f1060addd3.
    
    * Revert "Merge branch 'codeclubjusl:main' into feat/dashboard"
    
    This reverts commit ceed1b057bcae2089d02f87b907c85a09d12b93b, reversing
    changes made to 1b7bd4befd31e82dbce478a05763d8f1060addd3.
    
    * feat: add browser redirects for react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,58 @@
-import './App.css'
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import MerchandisePage from './components/MerchandisePage'
-import Hero from './components/home/Hero'
-import Timeline from './components/home/Timeline'
-import About from './components/home/About'
-import Contact from './components/home/Contact'
-import Speakers from './components/home/Speakers'
-import Sponsors from './components/home/Sponsors'
-import Gallery from './components/home/Gallery'
-import Eventpage from "./components/Events/Eventpage"
-import EventRegistration from './components/Events/EventRegistration'
+import "./App.css";
+import { LandingPage } from "./components/home/LandingPage";
+import MerchandisePage from "./components/MerchandisePage";
+import Eventpage from "./components/Events/Eventpage";
+import EventRegistration from "./components/Events/EventRegistration";
+import { DashboardPage } from "./components/protected_routes/DashboardPage";
+
+import { ProtectedRoute } from "./components/protected_routes/AuthRoutes";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  /**
+   * @typedef {object} User
+   * @property {string} id
+   * @property {string} name
+   * @property {string[]} permissions
+   * @property {string} [imageURL]
+   * @property {string} year
+   * @property {string} department
+   */
+
+  /**
+   * Tracks the current user as per authentication state
+   * @type {[User, React.Dispatch<React.SetStateAction<User>>]}
+   */
+  const [user, setUser] = useState({
+    id: "123",
+    name: "bob pop",
+    permissions: [],
+    department: "idk man",
+    year: "Eighth",
+  });
+
+  const handleLogout = () => setUser(null);
+
   return (
     <>
       <Router>
         <Routes>
+          <Route index element={<LandingPage />} />
+          <Route element={<ProtectedRoute accessAllowed={!!user} />}>
+            <Route
+              path="/dashboard"
+              element={<DashboardPage user={user} logout={handleLogout} />}
+            />
+          </Route>
           <Route path="/merchandise" element={<MerchandisePage />} />
-          <Route path="/events" element={<Eventpage />} /> 
-          <Route path="/eventregistration" element={<EventRegistration />} /> 
-          <Route path="/" element={
-            <div>
-              <main className='font-sometypeMono'>
-                <Navbar />
-                <Hero />
-                <Timeline />
-                <Sponsors />
-                <Speakers />
-                <About />
-                <Gallery />
-                <Contact />
-                <Footer />
-              </main>
-            </div>
-          } />
+          <Route path="/events" element={<Eventpage />} />
+          <Route path="/eventregistration" element={<EventRegistration />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
