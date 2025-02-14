f8b5364f6cdec723ec98cdd22dec38c22e4803df
rahul-p19 <pandeyrahul.work@gmail.com>
2025-02-14 17:50:27 +0000

    gallery images updated to webp

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -9,31 +9,31 @@ gsap.registerPlugin(useGSAP, ScrollTrigger, CustomEase);
 
 
 const photos = [
-  "/gallery/1.png",
-  "/gallery/2.png",
-  "/gallery/3.png",
-  "/gallery/4.png",
-  "/gallery/5.png",
-  "/gallery/6.png",
-  "/gallery/7.png",
-  "/gallery/8.png",
-  "/gallery/9.png",
-  "/gallery/10.png",
-  "/gallery/11.png",
-  "/gallery/12.png",
-  "/gallery/13.png",
-  "/gallery/14.png",
-  "/gallery/15.png",
-  "/gallery/16.png",
-  "/gallery/17.png",
-  "/gallery/18.png",
-  "/gallery/19.png",
-  "/gallery/20.png",
-  "/gallery/21.png",
-  "/gallery/22.png",
-  "/gallery/23.png",
-  "/gallery/24.png",
-  "/gallery/25.png",
+  "/gallery/1.webp",
+  "/gallery/2.webp",
+  "/gallery/3.webp",
+  "/gallery/4.webp",
+  "/gallery/5.webp",
+  "/gallery/6.webp",
+  "/gallery/7.webp",
+  "/gallery/8.webp",
+  "/gallery/9.webp",
+  "/gallery/10.webp",
+  "/gallery/11.webp",
+  "/gallery/12.webp",
+  "/gallery/13.webp",
+  "/gallery/14.webp",
+  "/gallery/15.webp",
+  "/gallery/16.webp",
+  "/gallery/17.webp",
+  "/gallery/18.webp",
+  "/gallery/19.webp",
+  "/gallery/20.webp",
+  "/gallery/21.webp",
+  "/gallery/22.webp",
+  "/gallery/23.webp",
+  "/gallery/24.webp",
+  "/gallery/25.webp",
 ];
 
 function Gallery() {
@@ -88,7 +88,7 @@ function Gallery() {
           <ul className='flex h-full gap-x-3 infinite-scroll-gallery border-greyBorder border-t border-b'>
             {[...photos, ...photos].map((photo, ind) => (
               <li key={ind} className='h-full w-[80vw] sm:w-[35vw] grid place-items-center transition-all duration-1000'>
-                <img src={photo} loading="lazy" className='h-full w-full aspect-video object-cover object-top' />
+                <img loading="lazy" src={photo} className='h-full w-full aspect-video object-cover object-top' />
               </li>
             ))}
           </ul>
