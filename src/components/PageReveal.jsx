5978abdc365f430fc9ba0e8cb83ab6e9b05429c6
rahul-p19 <pandeyrahul.work@gmail.com>
2025-02-18 06:11:53 +0000

    fix: update loader image and increase animation time

--- src/components/PageReveal.jsx
+++ src/components/PageReveal.jsx
@@ -17,21 +17,21 @@ function PageReveal() {
     gsap.to([logo.current, loader.current, technoManagementFest.current], {
       opacity: 0,
       display: "none",
-      duration: 0.3,
+      duration: 0.5,
       delay: 0.3
     })
 
     gsap.to(leftBlock.current, {
       xPercent: -120,
-      delay: 0.7,
-      duration: 1.5,
+      delay: 0,
+      duration: 2.5,
       ease: "power3.inOut"
     })
 
     gsap.to(rightBlock.current, {
       xPercent: 120,
-      delay: 0.7,
-      duration: 1.5,
+      delay: 0,
+      duration: 2.5,
       ease: "power3.inOut"
     })
 
@@ -49,10 +49,7 @@ function PageReveal() {
       <div ref={leftBlock} className='absolute bg-background h-screen w-full right-[45%]'></div>
       <div ref={rightBlock} className='absolute bg-background h-screen w-full left-[45%]'></div>
       <img ref={logo} src="/fetsu-presents-srijan25.svg" alt="Srijan 25 Logo" className="absolute top-[30%] left-[51%] -translate-[50%] h-36 w-auto" />
-      <div ref={technoManagementFest} className="text-center absolute top-[52.5%] left-[50%] -translate-[50%] border-greyBorder border p-3 flex flex-col w-4/5 sm:w-auto font-sans">
-        <h3 className="md:text-xl">The Annual Techno-Management Fest of</h3>
-        <h2 className="text-xl md:text-3xl font-bold">JADAVPUR UNIVERSITY</h2>
-      </div>
+      <img src="/techno-management-fest.webp" alt="The Annual Techno-Management Fest of Jadavpur University" ref={technoManagementFest} className="text-center absolute top-[52.5%] left-[50%] -translate-[50%] border border-greyBorder p-2 w-2/3 sm:w-1/3" />
       <div ref={loader} className="absolute top-[75%] left-[50%] -translate-[50%] p-3 animate-spin bg-gradient-to-bl from-red via-purple to-lavender h-24 w-24 aspect-square rounded-full">
         <div className="rounded-full h-full w-full bg-background"></div>
       </div>
--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -17,11 +17,8 @@ import Artists from "./Artists";
 function Loading() {
   return <div className="h-screen w-screen bg-background fixed z-[300]">
     <img src="/fetsu-presents-srijan25.svg" alt="Srijan 25 Logo" className="absolute top-[30%] left-[51%] -translate-[50%] h-36 w-auto" />
-    <div className="text-center absolute top-[52.5%] left-[50%] -translate-[50%] border-greyBorder border p-3 flex flex-col w-4/5 sm:w-auto font-sans">
-      <h3 className="md:text-xl">The Annual Techno-Management Fest of</h3>
-      <h2 className="text-xl md:text-3xl font-bold">JADAVPUR UNIVERSITY</h2>
-    </div>
     <div className="absolute top-[75%] left-[50%] -translate-[50%] p-3 animate-spin bg-gradient-to-bl from-red via-purple to-lavender h-24 w-24 aspect-square rounded-full">
+      <img src="/techno-management-fest.webp" alt="The Annual Techno-Management Fest of Jadavpur University" className="text-center absolute top-[52.5%] left-[50%] -translate-[50%] border border-greyBorder p-2 w-2/3 sm:w-1/3" />
       <div className="rounded-full h-full w-full bg-background"></div>
     </div>
   </div>
