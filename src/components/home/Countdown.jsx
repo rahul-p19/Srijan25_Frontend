bf2b8521631435ce0b52ac1616eb756c640792fd
rahul-p19 <pandeyrahul.work@gmail.com>
2025-02-27 14:40:06 +0000

    feat: add countdown

---
+++ src/components/home/Countdown.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+export default function Countdown() {
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const eventDate = new Date("2025-03-20T00:00:00").getTime();
+
+    const timer = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = eventDate - now;
+
+      setTimeLeft({
+        days: Math.floor(distance / (1000 * 60 * 60 * 24)) > 0 ? Math.floor(distance / (1000 * 60 * 60 * 24)) : 0,
+        hours: Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ) > 0 ? Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ) : 0,
+        minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) > 0 ? Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) : 0,
+        seconds: Math.floor((distance % (1000 * 60)) / 1000) > 0 ? Math.floor((distance % (1000 * 60)) / 1000) : 0,
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <>
+      <div className="bg-transparent flex flex-col items-center justify-center space-y-4 px-0.5 backdrop-blur-lg w-full h-full font-sometypeMono ">
+
+        <div className="grid lg:grid-cols-4 grid-cols-4 gap-3 sm:gap-4 text-center">
+          {Object.entries(timeLeft).map(([unit, value]) => (
+            <div
+              key={unit}
+              className="flex flex-col items-center justify-center"
+            >
+              <span className="text-xl text-transparent bg-clip-text bg-gradient-to-r from-purple to-red">
+                {value.toString().padStart(2, "0")}
+              </span>
+
+              <span className="text-sm text-white capitalize hidden sm:block">{unit}</span>
+              <span className="text-sm text-white capitalize sm:hidden">{unit.charAt(0)}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,6 +1,7 @@
 import "./hero.css";
 import React, { Suspense, lazy, useRef } from 'react'
 import { Link } from "react-router-dom";
+import Countdown from "./Countdown";
 
 const LazyMascotAnimation = lazy(() => import("./MascotAnimation"))
 
@@ -42,7 +43,10 @@ function Hero() {
           <h2 className='uppercase font-bold text-2xl text-center'>Jadavpur University</h2>
         </div>
       </div>
-      <div className='absolute p-3 z-[90] bottom-[47%] sm:bottom-[20%] right-[3%] sm:right-[12%] bg-background border border-greyBorder'>
+      <div className='absolute p-3 z-[90] bottom-[46.5%] sm:bottom-[30%] right-[55%] sm:right-[8%] bg-background border border-greyBorder sm:border-b-transparent sm:w-1/4'>
+        <Countdown />
+      </div>
+      <div className='absolute p-3 z-[90] bottom-[46.5%] sm:bottom-[20%] right-[3%] sm:right-[8%] bg-background border border-greyBorder sm:w-1/4'>
         <h2 className='text-base sm:text-lg md:text-xl lg:text-3xl'><span className='text-transparent bg-clip-text bg-gradient-to-r from-blue via-red to-red'>20-23</span> MARCH <br className='sm:hidden' /> 2025</h2>
       </div>
       {/*<img src='/mascot.svg' className='absolute z-[100] top-[32%] sm:top-[20%] left-[50%] -translate-x-[50%] h-3/4 mascot-animation'
