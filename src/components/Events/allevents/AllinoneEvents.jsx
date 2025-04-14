b79f0193eb203d4ee9fd2edb1f7d52ee82155c4e
Arnob-B <arnobbhakta00@gmail.com>
2025-04-15 00:23:31 +0530

    registration deadline added

--- src/components/Events/allevents/AllinoneEvents.jsx
+++ src/components/Events/allevents/AllinoneEvents.jsx
@@ -292,7 +292,7 @@ const AllEvents = () => {
 
             {/* Registration Deadline */}
             <h2 className="font-bold text-center mb-4 text-red-700 text-xl md:text-2xl">
-              LAST DATE FOR REGISTRATION: TBD
+              LAST DATE FOR REGISTRATION: {eventData.registrationDeadline ? eventData.registrationDeadline : "TBD"}
             </h2>
 
             {/* Grid for Left & Right Sections */}
