bdf59a15c87c08c46ad4de36d67ad75f76ded991
rahul-p19 <pandeyrahul.work@gmail.com>
2025-05-10 09:30:56 +0530

    feat: add 2025 sponsors

--- src/components/home/Sponsors.jsx
+++ src/components/home/Sponsors.jsx
@@ -1,5 +1,5 @@
 import "./infiniteScroll.css"
-import React, { useState, useRef } from 'react'
+import { useState, useRef } from 'react'
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
@@ -8,30 +8,26 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 
 const currentSponsors = [
-  {
-    image: "/fetsu-presents-srijan25.svg",
-    text: ""
-  },
-  {
-    image: "/fetsu-presents-srijan25.svg",
-    text: ""
-  },
-  {
-    image: "/fetsu-presents-srijan25.svg",
-    text: ""
-  },
-  {
-    image: "/fetsu-presents-srijan25.svg",
-    text: ""
-  },
-  {
-    image: "/fetsu-presents-srijan25.svg",
-    text: ""
-  },
-  {
-    image: "/fetsu-presents-srijan25.svg",
-    text: ""
-  },
+  { image: "aliff-logo.webp", text: "Aliff", contrast: false},
+  { image: "bong-pizza.webp", text: "Bong Pizza", contrast: false},
+  { image: "calcutta-cacophony-logo.webp", text: "Calcutta Cacophony", contrast: false},
+  { image: "canara-bank-logo.webp", text: "Canara Bank", contrast: false},
+  { image: "comedy-company-logo.webp", text: "Comedy Company", contrast: false},
+  { image: "drivers-club-logo.webp", text: "Classic Drivers Club", contrast: false},
+  { image: "eve-placement-logo.webp", text: "EvePlacement", contrast: false},
+  { image: "evepaper-logo.webp", text: "Evepaper", contrast: false},
+  { image: "friends-fm-logo.webp", text: "91.9 Friends FM", contrast: false},
+  { image: "ims-logo.webp", text: "IMS", contrast: false},
+  { image: "interview-buddy.webp", text: "Interview Buddy", contrast: true},
+  { image: "kitkat-logo.webp", text: "KitKat", contrast: false},
+  { image: "nescafe-logo.webp", text: "Nescafe", contrast: false},
+  { image: "redbull-logo.webp", text: "Red Bull", contrast: false},
+  { image: "spykar-logo.webp", text: "Spykar", contrast: false},
+  { image: "sugoi-drop-logo.webp", text: "SugoiDrop", contrast: true},
+  { image: "telegraph-logo.webp", text: "Telegraph", contrast: false},
+  { image: "toi-logo.webp", text: "Times of India", contrast: false},
+  { image: "unstop-logo.webp", text: "Unstop", contrast: false},
+  { image: "wiley-logo-black.webp", text: "Wiley", contrast: true},
 ]
 
 const pastSponsors = [
@@ -72,25 +68,18 @@ const pastSponsors = [
 
 const primarySponsors = [
   {
-    image: "/fetsu-presents-srijan-glitch.svg",
-    text: "Primary Sponsor",
-    link: ""
+    image: "/mio-amore-logo.svg",
+    text: "Mio Amore - Premium Sponsor",
   },
   {
-    image: "/fetsu-presents-srijan-glitch.svg",
-    text: "Primary Sponsor",
-    link: ""
-  },
-  {
-    image: "/fetsu-presents-srijan-glitch.svg",
-    text: "Primary Sponsor",
-    link: ""
-  },
+    image: "/paharpur-logo.webp",
+    text: "Paharpur Cooling Towers - Associate Partner",
+  }
 ]
 
 function Sponsors() {
 
-  const [heading, setHeading] = useState("Past Sponsors");
+  const [heading, setHeading] = useState("Sponsors");
   const container = useRef();
 
   useGSAP(() => {
@@ -149,16 +138,15 @@ function Sponsors() {
 
   return (
     <div ref={container} className='w-full relative border-greyBorder border-t grid grid-cols-1 sm:grid-cols-5 grid-rows-5 sm:grid-rows-1 h-[80vh] overflow-x-hidden'>
-      <div className={`absolute top-[40%] sm:top-4 right-0 h-1/4 w-full sm:w-4/5 flex justify-between items-center ${heading === "Sponsors" ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-opacity duration-500`}>
+      <div className={`absolute top-[33%] sm:top-0 right-0 h-1/3 w-full sm:w-4/5 flex justify-center sm:justify-between items-center ${heading === "Sponsors" ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-opacity duration-500`}>
         <div className="grid place-items-center grid-cols-2 w-2/3 sm:w-3/4 h-full gap-4 px-2">
-          {primarySponsors.filter((sponsor, ind) => ind < 2).map((sponsor, ind) => <img alt={sponsor.text} src={sponsor.image} key={ind} height="auto" width="auto" className="sm:w-1/2 border border-greyBorder px-2" />)}
+          {primarySponsors.filter((sponsor, ind) => ind < 2).map((sponsor, ind) => <img alt={sponsor.text} src={`/sponsors/${sponsor.image}`} key={ind} height="auto" width="auto" className="h-2/5 sm:h-3/4 w-auto" />)}
         </div>
-        <div className="grid place-items-center h-full w-1/3 sm:w-1/4 p-2 sm:p-4"><img alt={primarySponsors[2].text} src={primarySponsors[2].image} height="auto" width="auto" className="w-48 border border-greyBorder px-2" /></div>
       </div>
       <div className='relative bg-background z-[100] h-full border-greyBorder border-r -mr-[1px] row-span-2'>
         <div className='sm:hidden absolute left-[50%] -translate-x-[50%] h-full border-greyBorder border-l border-r w-5/7'></div>
         <h2 className='scrollAnimatedText absolute text-2xl sm:text-xl xl:text-3xl text-center top-[50%] left-[50%] -translate-[50%]'>{heading}</h2>
-        <div className='hidden absolute top-[65%] left-[50%] -translate-x-[50%] gap-x-3'>
+        <div className='absolute top-[65%] left-[50%] -translate-x-[50%] gap-x-3'>
           <button onClick={() => setHeading("Past Sponsors")}
             className={`text-7xl font-bold ${heading === "Past Sponsors" ? 'opacity-40' : ''}`}>
             &lsaquo;
@@ -173,11 +161,11 @@ function Sponsors() {
       <div className='relative col-span-3 border-greyBorder border-t sm:border-t-transparent sm:border-l sm:border-r grid grid-cols-2 row-span-3 h-full'>
         <div className='sm:hidden absolute left-[50%] -translate-x-[50%] h-full border-greyBorder border-l border-r w-5/7'></div>
         <div className='border-greyBorder sm:border-r'></div>
-        <div className='absolute top-[50%] -translate-y-[50%] flex h-1/2'>
+        <div className='absolute top-[60%] -translate-y-[50%] flex h-1/2'>
           <ul className='flex items-center infinite-scroll-carousel bg-background border-greyBorder border-t border-b h-full'>
             {heading === "Sponsors" ? [...currentSponsors, ...currentSponsors].map((sponsor, ind) => (
               <li key={ind} className='w-[40vw] sm:w-[30vw] h-full px-4 sm:px-0 grid place-items-center transition-all duration-1000s border-greyBorder border-l'>
-                <img loading="lazy" src={sponsor.image} className='w-72 py-6' alt="Sponsor of Srijan'25" width="auto" height="auto" />
+                <img loading="lazy" src={`/sponsors/${sponsor.image}`} className={`h-1/2 w-1/2 object-contain ${sponsor.contrast ? 'p-2 bg-white rounded-sm' : ''}`} alt={sponsor.text} width="auto" height="auto" />
               </li>
             )) : [...pastSponsors, ...pastSponsors].map((sponsor, ind) => (
               <li key={ind} className='w-[40vw] sm:w-[30vw] px-4 sm:px-0 grid place-items-center transition-all duration-1000s border-greyBorder border-l h-full'>
